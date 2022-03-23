2eeaf9affe8e011250b1ef100824c6052aa9db4a
遠藤貴文 <>
2022-03-23 20:31:09 +0900

    categoryテーブル作成

---
+++ test/models/category_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  foodclass  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  recipe_id  :bigint           not null
+#
+# Indexes
+#
+#  index_categories_on_recipe_id  (recipe_id)
+#
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
