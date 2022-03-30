30ea17e3795f84b3842bcd0e58e12d8e10951e88
遠藤貴文 <>
2022-03-31 06:59:55 +0900

    new, create アクション作成

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -7,6 +7,17 @@ class AssessmentsController < ApplicationController
     end
 
     def create
-        
+        recipe = Recipe.find(params[:recipe_id])
+        @assessment = recipe.build_assessment(assessment_params)
+        if @assessment.save
+            redirect_to recipe_path(recipe.id)
+        else
+            render :new
+        end
     end
+
+    private
+        def assessment_params
+            params.require(:assessment).permit(:taste, :easily, :cost, :revenge)
+        end
 end
