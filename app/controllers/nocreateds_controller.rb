8140421765f870f02fcb5750c3b2d19247199749
遠藤貴文 <>
2022-04-11 07:34:50 +0900

    not-created ページの作成

--- app/controllers/nocreateds_controller.rb
+++ app/controllers/nocreateds_controller.rb
@@ -2,6 +2,6 @@ class NocreatedsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-
+        @recipes = Recipe.left_outer_joins(:assessment).where(assessments: { id: nil })
     end
 end
