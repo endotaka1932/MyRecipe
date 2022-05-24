cb367fc42fa74f85f1337f992844212a52517f7b
遠藤貴文 <>
2022-05-24 20:58:41 +0900

    レシピ一覧の表示を作成日の降順に変更(未作成、リベンジを除く)

--- app/controllers/categorys/rice_controller.rb
+++ app/controllers/categorys/rice_controller.rb
@@ -3,6 +3,6 @@ class Categorys::RiceController < Categorys::ApplicationController
     def show
         category = Category.find_by(foodclass: 'ライス')
         category_id = category.id
-        @recipes = RecipeCategory.where(category_id: category_id).includes(:recipe)
+        @recipes = RecipeCategory.where(category_id: category_id).includes(:recipe).order(created_at: :DESC)
     end
 end
