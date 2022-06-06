41b30e8a68119abf01c2ae26ccf5a1116e0600e6
遠藤貴文 <>
2022-06-07 08:07:20 +0900

    request spec

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
-    factory:recipe do
+    factory :recipe do
         title { Faker::Lorem.characters(number: 10) }
+        association :user, factory: :user
+
+        trait :recipe_with_category do
+            after :create do
+                create(:recipe_category, recipe: create(:recipe), category: create(:category))
+            end
+        end
     end
 end
