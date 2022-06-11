7204954b773b1c54e6ff4659376ab16e2332584d
遠藤貴文 <>
2022-06-11 12:05:15 +0900

    heroku dotenvのエラー

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module MyRecipe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    Bundler.require(*Rails.groups)
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
 
-    Dotenv::Railtie.load
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
